433fee2e4be0def0d0e06634c5a2e912a1d3cd76
Rotimi Best <best@classroomio.com>
2024-12-02 14:42:05 +0100

    feat: deploy api

--- main.ts
+++ main.ts
@@ -1,1 +1,40 @@
-console.log('Hello, world!');
+/// <reference lib="deno.ns" />
+import { Hono } from 'npm:hono';
+import { cors } from 'npm:hono/cors';
+import { cache } from 'npm:hono/cache';
+
+import RepositoryHandler from './routes/repository.ts';
+
+import 'jsr:@std/dotenv/load';
+
+const PORT = Number(Deno.env.get('PORT')) || 8000;
+
+const app = new Hono();
+
+/** MIDDLEWARES */
+app.use('/api/*', cors());
+
+app.get(
+  '/api/*',
+  cache({
+    cacheName: 'naijastars-api',
+    cacheControl: 'max-age=3600',
+    wait: true,
+  })
+);
+
+/** ROUTES */
+// Root route
+app.get('/', (c) => {
+  return c.html(
+    `<h4>Naija Stars API by <a href="https://classroomio.com">ClassroomIO</a></h4>`
+  );
+});
+
+// Repository routes
+app.route('/api', RepositoryHandler);
+
+/** START THE SERVER */
+console.log(`API server running on http://localhost:${PORT}`);
+
+Deno.serve({ port: PORT }, app.fetch);
