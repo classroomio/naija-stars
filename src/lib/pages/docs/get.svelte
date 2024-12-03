9b7e8e08205a8d48b76feb88139c95f696df5e66
Adetunji Adeyemi <tvnji01@gmail.com>
2024-12-03 17:17:39 +0100

    feat: docs page

---
+++ src/lib/pages/docs/get.svelte
@@ -0,0 +1,116 @@
+<section class="flex item-start gap-5 p-5">
+  <section class="w-2/4">
+    <h1 class="text-xl">Get Repositories</h1>
+    <p class="text-sm mt-1">Get Repositories from the database</p>
+
+    <div class="border rounded-md p-2.5 my-5 text-sm">
+      <span class="bg-green-900 text-green-500 px-2 py-1 text-sm rounded-sm">GET</span>
+      <span class="tracking-wide ml-1"> / api / repositories</span>
+    </div>
+
+    <h1 class="text-xl">Request</h1>
+    <p class="text-sm mt-2">Query Parameters</p>
+
+    <div class="≈p-2.5 mt-10 text-sm">
+      <div class="flex items-center gap-5 mb-4">
+        <p>Page</p>
+        <p class="text-xs px-2 py-0.5 bg-red-50 text-black rounded-md">number</p>
+      </div>
+
+      <p>Page number of repository</p>
+    </div>
+
+    <div class="≈p-2.5 mt-10 text-sm">
+      <div class="flex items-center gap-5 mb-4">
+        <p>Limit</p>
+        <p class="text-xs px-2 py-0.5 bg-red-50 text-black rounded-md">number</p>
+      </div>
+
+      <p>Limit per page</p>
+    </div>
+
+    <div class="≈p-2.5 mt-10 text-sm">
+      <div class="flex items-center gap-5 mb-4">
+        <p>SortBy</p>
+        <p class="text-xs px-2 py-0.5 bg-red-50 text-black rounded-md">string</p>
+      </div>
+
+      <p>Sort by stars | forks</p>
+    </div>
+
+    <div class="≈p-2.5 mt-10 text-sm">
+      <div class="flex items-center gap-5 mb-4">
+        <p>Order</p>
+        <p class="text-xs px-2 py-0.5 bg-red-50 text-black rounded-md">string</p>
+      </div>
+
+      <p>Order of sorting. asc | desc</p>
+    </div>
+  </section>
+
+  <section class="w-2/4 flex flex-col gap-5">
+    <div
+      class="flex flex-col gap-3 bg-black dark:bg-transparent dark:border text-white rounded-md h-auto p-4 text-xs"
+    >
+      <p>curl --request GET \</p>
+      <p>--url http://localhost:8000/api/repositories \</p>
+    </div>
+
+    <div
+      class="bg-black dark:bg-transparent dark:border text-white rounded-md h-[70%] overflow-auto"
+    >
+      <pre>
+        <code class="language-json text-xs">
+          {`{
+  "status": "success",
+  "data": [   [
+    {
+        "id": 34,
+        "name": "chakra-ui",
+        "link": "https://github.com/chakra-ui/chakra-ui",
+        "description": "⚡️ Simple, Modular & Accessible UI Components for your React Applications",
+        "author": "chakra-ui",
+        "author_link": "https://github.com/chakra-ui",
+        "author_avatar": "https://avatars.githubusercontent.com/u/54212428?v=4",
+        "stars": 38073,
+        "topics": [
+            "a11y",
+            "accessible",
+            "ark-ui",
+            "chakra-ui",
+            "component",
+            "css-in-js",
+            "dark-mode",
+            "design-system",
+            "react",
+            "react-components",
+            "reactjs",
+            "styled",
+            "ui-components",
+            "ui-library",
+            "uikit",
+            "wai-aria"
+        ],
+        "license": {
+            "key": "mit",
+            "url": "https://api.github.com/licenses/mit",
+            "name": "MIT License"
+        },
+        "forks": 3285,
+        "open_issues_count": 12,
+        "archived": false,
+        "disabled": false,
+        "original_created_at": "2019-08-17T14:27:54.000Z",
+        "original_updated_at": "2024-12-03T07:58:40.000Z",
+        "created_at": "2024-12-01T14:44:03.981Z",
+        "updated_at": "2024-12-03T10:04:43.429Z",
+        "author_link_madeinnigeria": "https://twitter.com/thesegunadebayo",
+        "author_madeinnigeria": "@thesegunadebayo"
+    }
+]
+}`}
+        </code>
+      </pre>
+    </div>
+  </section>
+</section>
--- src/lib/pages/docs/page.svelte
+++ src/lib/pages/docs/page.svelte
@@ -1,1 +1,66 @@
-<section class="flex justify-center items-center">hiii</section>
+<script>
+  import { Router } from 'svelte-routing';
+  import GetPage from './get.svelte';
+  import SearchPage from './search.svelte';
+  import logo from '$lib/assets/logo.svg';
+
+  export let url = '';
+
+  let state = '';
+
+  const navigation = [
+    {
+      label: 'get'
+    },
+    {
+      label: 'search'
+    }
+  ];
+</script>
+
+<Router {url}>
+  <main class="p-5">
+    <div
+      class="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[100px_minmax(0,1fr)] lg:gap-10"
+    >
+      <aside
+        class="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block"
+      >
+        <div class="">
+          <button on:click={() => (state = '')}>Repositories</button>
+          <ul>
+            {#each navigation as nav}
+              <li class="py-1">
+                <button
+                  on:click={() => (state = nav.label)}
+                  class="px-2 py-1 text-xs rounded-sm {nav.label === 'get' &&
+                    'bg-green-900 text-green-400'} {nav.label === 'search' &&
+                    'bg-gray-900 text-white'}"
+                >
+                  <span class="uppercase">
+                    {nav.label}
+                  </span>
+                </button>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      </aside>
+
+      <div class=" h-full">
+        {#if state === ''}
+          <section
+            class="flex flex-col justify-center items-center gap-5 rounded-md border h-[70%]"
+          >
+            <h1 class="text-4xl">Welcome to Naija Stars Docs</h1>
+            <img src={logo} alt="logo" class="w-28 h-28" />
+          </section>
+        {:else if state === 'get'}
+          <GetPage />
+        {:else if state === 'search'}
+          <SearchPage />
+        {/if}
+      </div>
+    </div>
+  </main>
+</Router>
